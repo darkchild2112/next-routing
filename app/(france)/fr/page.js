dcab13f0c7cfd99231aafd71dbcf98ccb740b8bb
Ben Abbott <ben.abbott@intoglobal.com>
2024-09-18 14:19:26 +0100

    routing

--- app/(france)/fr/page.js
+++ app/(france)/fr/page.js
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1>Hello World From Vercel</h1>
+    <h1>France Specific</h1>
   );
 }
---
+++ app/(global)/[locale]/page.js
@@ -0,0 +1,5 @@
+export default function Home({ params }) {
+  return (
+    <h1>Generic Locale ({params.locale})</h1>
+  );
+}
